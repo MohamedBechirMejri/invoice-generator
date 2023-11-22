f8318e7fc5551a58ed001ae2ca8fabecabce644e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-11-23 00:11:59 +0100

    Update div2png function to include invoice number in downloaded image name

--- lib/div2png.ts
+++ lib/div2png.ts
@@ -1,11 +1,11 @@
 import html2canvas from "html2canvas";
 
-export const div2png = (div: HTMLElement) => {
+export const div2png = (div: HTMLElement, number: number) => {
   html2canvas(div).then(canvas => {
     const img = canvas.toDataURL("image/png");
     const link = document.createElement("a");
     link.href = img;
-    link.download = "div.png";
+    link.download = `invoice#${number}.png`;
     link.click();
   });
 };

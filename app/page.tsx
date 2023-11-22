ae209b68084d727c4c2a7802a4958fbdc5bf8a24
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-11-22 23:04:09 +0100

    Refactor page layout and styling

--- app/page.tsx
+++ app/page.tsx
@@ -49,100 +49,102 @@ export default function Home() {
   const [dueDate, setDueDate] = useState("OCTOBER 1, 2021");
 
   return (
-    <div
-      className="grid grid-cols-[7rem,35rem] h-max border w-max"
-      style={firaCode.style}
-    >
-      <header className="relative bg-gradient-to-tl from-green-200 to-blue-200 grid grid-rows-2 grid-cols-1 place-items-center w-[7rem]">
-        <div className="min-w-max -rotate-90 font-semibold text-base text-right">
-          <p>NO. {number}</p>
-          <p>Issued on {date}</p>
-        </div>
-        <div className="min-w-max -rotate-90 font-[400] text-base uppercase">
-          <p>
-            {website} • {phone}
-          </p>
-          <p>{address}</p>
-        </div>
-      </header>
-      <main className="flex flex-col justify-between min-h-screen p-8 bg-[#fffffe]">
-        {/* Header Section */}
-        <section className="grid grid-cols-[1fr,auto,1fr] gap-8">
-          {/* keep this div */}
-          <div></div>
-          <h1
-            className="text-7xl font-bold mt-8"
-            style={loveYaLikeASister.style}
-          >
-            Invoice
-          </h1>
-
-          <div className="text-center text-xs bg-gradient-to-tl from-green-200 to-blue-200 rounded-full w-24 h-24 flex items-center overflow-hidden">
-            {logo ? (
-              <Image
-                src={logo}
-                alt="logo"
-                className="w-full h-full object-contain"
-                width={96}
-                height={96}
-              />
-            ) : (
-              <p>Your logo here!</p>
-            )}
+    <div className="grid grid-cols-[auto,1fr] gap-4">
+      <div
+        className="grid grid-cols-[7rem,35rem] h-max border w-max"
+        style={firaCode.style}
+      >
+        <header className="relative bg-gradient-to-tl from-green-200 to-blue-200 grid grid-rows-2 grid-cols-1 place-items-center w-[7rem]">
+          <div className="min-w-max -rotate-90 font-semibold text-base text-right">
+            <p>NO. {number}</p>
+            <p>Issued on {date}</p>
           </div>
-        </section>
+          <div className="min-w-max -rotate-90 font-[400] text-base uppercase">
+            <p>
+              {website} • {phone}
+            </p>
+            <p>{address}</p>
+          </div>
+        </header>
+        <main className="flex flex-col justify-between min-h-screen p-8 bg-[#fffffe]">
+          {/* Header Section */}
+          <section className="grid grid-cols-[1fr,auto,1fr] gap-8">
+            {/* keep this div */}
+            <div></div>
+            <h1
+              className="text-7xl font-bold mt-8"
+              style={loveYaLikeASister.style}
+            >
+              Invoice
+            </h1>
 
-        <hr className="bg-gradient-to-tl from-green-200 to-blue-200 h-2 rounded mt-6 mb-4" />
-
-        {/* Billing Section */}
-        <section className="my-4 text-xs">
-          <h2 className="text-lg font-semibold mb-2">BILLED TO</h2>
-          <p className="font-semibold text-3xl uppercase tracking-wider">
-            {cname}
-          </p>
-          <p>{cphone}</p>
-          <p>{cemail}</p>
-          <p>{caddress}</p>
-        </section>
-
-        {/* Description Section */}
-        <section className="pt-6">
-          <h3 className="text-lg font-bold flex justify-between">
-            <span>DESCRIPTION OF ITEM</span> <span>PRICE</span>
-          </h3>
-          <hr className="bg-gradient-to-tl from-green-200 to-blue-200 h-1 rounded my-2" />
-          {items.map(item => (
-            <div key={item.item} className="flex justify-between">
-              <p>{item.item}</p>
-              <p>{item.price}</p>
+            <div className="text-center text-xs bg-gradient-to-tl from-green-200 to-blue-200 rounded-full w-24 h-24 flex items-center overflow-hidden">
+              {logo ? (
+                <Image
+                  src={logo}
+                  alt="logo"
+                  className="w-full h-full object-contain"
+                  width={96}
+                  height={96}
+                />
+              ) : (
+                <p>Your logo here!</p>
+              )}
             </div>
-          ))}
-          <hr className="bg-gradient-to-tl from-green-200 to-blue-200 h-1 rounded mt-2" />
-        </section>
+          </section>
 
-        {/* Total Amount Section */}
-        <section className="flex justify-between -mt-4">
-          <h2 className="text-lg font-bold">TOTAL AMOUNT DUE</h2>
-          <p>{total}</p>
-        </section>
+          <hr className="bg-gradient-to-tl from-green-200 to-blue-200 h-2 rounded mt-6 mb-4" />
 
-        {/* Payment Details Section */}
-        <section className="my-6">
-          <h2 className="text-lg font-bold">PAYMENT DETAILS</h2>
-          <p>{accountName}</p>
-          <p>{bank}</p>
-          <p>Account Number: {accountNumber}</p>
-          <p>Routing Number: {routingNumber}</p>
-        </section>
+          {/* Billing Section */}
+          <section className="my-4 text-xs">
+            <h2 className="text-lg font-semibold mb-2">BILLED TO</h2>
+            <p className="font-semibold text-3xl uppercase tracking-wider">
+              {cname}
+            </p>
+            <p>{cphone}</p>
+            <p>{cemail}</p>
+            <p>{caddress}</p>
+          </section>
 
-        {/* Footer Section */}
-        <footer className="text-center flex items-center justify-between w-full">
-          <p className="font-medium">*DUE BY {dueDate}</p>
-          <p style={puppiesPlay.style} className="text-7xl">
-            Thank you!
-          </p>
-        </footer>
-      </main>
+          {/* Description Section */}
+          <section className="pt-6">
+            <h3 className="text-lg font-bold flex justify-between">
+              <span>DESCRIPTION OF ITEM</span> <span>PRICE</span>
+            </h3>
+            <hr className="bg-gradient-to-tl from-green-200 to-blue-200 h-1 rounded my-2" />
+            {items.map(item => (
+              <div key={item.item} className="flex justify-between">
+                <p>{item.item}</p>
+                <p>{item.price}</p>
+              </div>
+            ))}
+            <hr className="bg-gradient-to-tl from-green-200 to-blue-200 h-1 rounded mt-2" />
+          </section>
+
+          {/* Total Amount Section */}
+          <section className="flex justify-between -mt-4">
+            <h2 className="text-lg font-bold">TOTAL AMOUNT DUE</h2>
+            <p>{total}</p>
+          </section>
+
+          {/* Payment Details Section */}
+          <section className="my-6">
+            <h2 className="text-lg font-bold">PAYMENT DETAILS</h2>
+            <p>{accountName}</p>
+            <p>{bank}</p>
+            <p>Account Number: {accountNumber}</p>
+            <p>Routing Number: {routingNumber}</p>
+          </section>
+
+          {/* Footer Section */}
+          <footer className="text-center flex items-center justify-between w-full">
+            <p className="font-medium">*DUE BY {dueDate}</p>
+            <p style={puppiesPlay.style} className="text-7xl">
+              Thank you!
+            </p>
+          </footer>
+        </main>
+      </div>
     </div>
   );
 }
